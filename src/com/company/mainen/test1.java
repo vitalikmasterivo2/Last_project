41bcb33856f6e2758d93c781eee49b1c07102738
vitalijmalisevich <vov560607@gmail.com>
2023-04-26 00:32:48 +0300

    Elf Problem commit

--- src/com/company/mainen/test1.java
+++ src/com/company/mainen/test1.java
@@ -2,20 +2,18 @@ package com.company.mainen;
 
 
 import com.company.center.CHero;
+import com.company.mps.Elf;
 import com.company.mps.Ork;
 
 public class test1 {
     public static void main(String[] args) {
     //Тестовий класс, де тестуються методи і т. д.
-        CHero ps1 = new CHero("vov", "human");
+        Elf ps1 = new Elf("Radiy", 3);
+        Ork ork = new Ork("lox");
 
-        Ork ork = new Ork("test");
+        ork.setHealth(ps1.ElfAttackOrk(ork.getHealth()));
 
-        ork.setHealth(ps1.AttackOrk(ork.getHealth()));
-        ork.setHealth(ps1.AttackOrk(ork.getHealth()));
-
-
+        System.out.println(ps1.toString());
         System.out.println(ork.toString());
-        ps1.tString();
     }
 }
